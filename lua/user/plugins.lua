569e28b825929c51b012078566ee10b3e3c9465a
Yazeed1s <106896327+Yazeed1s@users.noreply.github.com>
2022-11-27 12:22:40 -0500

    update plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,16 +4,16 @@ local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 ---@diagnostic disable-next-line: missing-parameter
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -27,25 +27,27 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+    return
 end
 
 -- Have packer use a popup window
 packer.init {
-  max_jobs = 50,
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-    prompt_border = "rounded", -- Border style of prompt popups.
-  },
+    max_jobs = 50,
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+        prompt_border = "rounded", -- Border style of prompt popups.
+    },
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-     -- Plugin Mangager
+    -- Plugin Mangager
     use "wbthomason/packer.nvim" -- Have packer manage itself
-    
+    use { "ellisonleao/gruvbox.nvim" }
+    use 'Yazeed1s/minimal.nvim'
+        -- Dashboard Init
     use {
         "simrat39/rust-tools.nvim",
         config = function()
@@ -87,11 +89,26 @@ return packer.startup(function(use)
         end,
         ft = { "rust", "rs" },
     }
+    use { 
+        'nguyenvukhang/nvim-toggler',
+        require('nvim-toggler').setup({
+        -- your own inverses
+        inverses = {
+            ['true'] = 'false',
+            ['false'] = 'true',
+            ['!='] = '==',
+            ['=='] = '!=',
+        },
+        -- removes the default <leader>i keymap
+        -- vim.keymap.set({ 'n', 'v' }, '<leader>cl', require('nvim-toggler').toggle)
+        remove_default_keybinds = false,
+        })
+    }
     use "p00f/clangd_extensions.nvim"
     -- Comment
     use "numToStr/Comment.nvim"
-      -- Quickfix
-    
+    -- Quickfix
+    -- use "RRethy/nvim-base16"
     use {
         "kevinhwang91/nvim-bqf",
         event = { "BufRead", "BufNew" },
@@ -127,26 +144,26 @@ return packer.startup(function(use)
         end,
     }
     -- using packer.nvim
-    use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'}
+    use { 'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons' }
     -- colorscheme
     use 'Yazeed1s/oh-lucy.nvim'
     use {
         "folke/trouble.nvim",
         requires = "kyazdani42/nvim-web-devicons",
         config = function()
-        require("trouble").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
         end
     }
     -- telescope
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-        }
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
     -- syntax
     -- Syntax/Treesitter
     use "nvim-treesitter/nvim-treesitter"
@@ -156,18 +173,17 @@ return packer.startup(function(use)
     use "nvim-treesitter/nvim-treesitter-textobjects"
     -- use "wellle/targets.vim"
     -- use "RRethy/nvim-treesitter-textsubjects"
-    use "kylechui/nvim-surround"
     use "windwp/nvim-autopairs"
     use {
         "abecodes/tabout.nvim",
-        requires = { "nvim-treesitter" }, 
+        requires = { "nvim-treesitter" },
     }
     use "lukas-reineke/indent-blankline.nvim"
     -- file manager
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
-                'nvim-tree/nvim-web-devicons', -- optional, for file icons
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
     }
 
@@ -181,20 +197,20 @@ return packer.startup(function(use)
     use "SmiteshP/nvim-navic"
     use "simrat39/symbols-outline.nvim"
     use "b0o/SchemaStore.nvim"
-    -- hints 
+    -- hints
     use "lvimuser/lsp-inlayhints.nvim"
-    -- 
+    --
     use({
         "kylechui/nvim-surround",
         tag = "*", -- Use for stability; omit to use `main` branch for the latest features
         config = function()
             require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+                -- Configuration here, or leave empty to use defaults
+            })
         end
     })
     -- nvim-cmp
-     -- Completion
+    -- Completion
     use "hrsh7th/nvim-cmp"
     use "hrsh7th/cmp-buffer" -- buffer completions
     use "hrsh7th/cmp-path" -- path completions
@@ -202,25 +218,24 @@ return packer.startup(function(use)
     use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/cmp-emoji"
     use "hrsh7th/cmp-nvim-lua"
-    use { "tzachar/cmp-tabnine", commit = "1a8fd2795e4317fd564da269cc64a2fa17ee854e", 
-    run = "./install.sh" }
+    use { "tzachar/cmp-tabnine", commit = "1a8fd2795e4317fd564da269cc64a2fa17ee854e",
+        run = "./install.sh" }
 
     -- git
     use {
         "lewis6991/gitsigns.nvim",
         requires = { "nvim-lua/plenary.nvim" },
-     }
+    }
     use "f-person/git-blame.nvim"
-    use "ruifm/gitlinker.nvim"
 
-      -- Keybinding
+    -- Keybinding
     use "folke/which-key.nvim"
     use { 'weilbith/nvim-code-action-menu' }
     -- Snippet
     use "L3MON4D3/LuaSnip" --snippet engine
     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-     -- Automatically set up your configuration after cloning packer.nvim
-     -- Put this at the end after all plugins
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
         require("packer").sync()
     end
