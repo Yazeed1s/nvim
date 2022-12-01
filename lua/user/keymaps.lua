c2bd26ae1207b539e1db147105f93e13216bbfcd
Yazeed1s <106896327+Yazeed1s@users.noreply.github.com>
2022-12-01 13:08:50 -0500

    keymap pwd

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,15 +1,18 @@
 -- keymaps
 local key_map = vim.api.nvim_set_keymap
 
-key_map("n", "jj", "<Esc>", { noremap = false })
-key_map("i", "jj", "<Esc>", { noremap = false })
+-- key_map("n", "jj", "<Esc>", { noremap = false })
 -- quit (or close window)
 key_map("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
--- write 
-key_map("n", "<Leader>w", ":w<CR>", { noremap = true})
+-- write
+key_map("n", "<Leader>w", ":w<CR>", { noremap = true })
 -- run packer sync
 key_map("n", "<leader>e", [[<Cmd>NvimTreeToggle<CR>]], { noremap = true, silent = true })
+-- key_map("n", "<leader>e", [[<Cmd>NERDTreeFocus<CR>]], { noremap = true, silent = true })
+key_map("n", "<leader>m", "[[<Cmd>cd %:p:h<CR>:pwd<CR>]]", { noremap = true, silent = true })
+
 -- Surround word under cursor w/ backticks (required vim-surround)
+--
 -- key_map("n", "<leader>{", "ysiW`", { noremap = false })
 -- Surround word under cursor w/ double quotes (required vim-surround)
 -- key_map("n", '<leader>"', 'ysiW"', { noremap = false })
